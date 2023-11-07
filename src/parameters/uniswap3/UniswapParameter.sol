3bbf3b8db0b70faabce5b54474908e920809f42a
Sam Sends <sbsends@gmail.com>
2023-11-06 16:02:14 -0800

    🚧  forge dep hell

--- src/parameters/uniswap3/UniswapParameter.sol
+++ src/parameters/uniswap3/UniswapParameter.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {ISwapRouter} from "v3-periphery/interfaces/ISwapRouter.sol";
 import {INonfungiblePositionManager} from "v3-periphery/interfaces/INonfungiblePositionManager.sol";
 import {TransferHelper} from "v3-periphery/libraries/TransferHelper.sol";
+import {IERC721Receiver} from "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract UniswapParameter is IERC721Receiver {
     ISwapRouter public immutable swapRouter;
@@ -22,8 +23,15 @@ contract UniswapParameter is IERC721Receiver {
     /* -------------------------------------------------------------------------- */
 
     function approveSwapRouter(address token, uint amount) {
-        // Approve the router to spend DAI.
         TransferHelper.safeApprove(token, address(swapRouter), amount);
+    }
+
+    function approvePositionManager(address token, uint amount) {
+        TransferHelper.safeApprove(
+            token,
+            address(nonfungiblePositionManager),
+            amount
+        );
     }
 
     /* -------------------------------------------------------------------------- */
@@ -60,5 +68,44 @@ contract UniswapParameter is IERC721Receiver {
     ) returns (uint256) {
         // optionally handle validation
         return swapRouter.exactOutput(params);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                             LIQUDITY PROVISION                             */
+    /* -------------------------------------------------------------------------- */
+
+    function mintLiquidityPosition(
+        INonfungiblePositionManager.MintParams memory params
+    )
+        returns (
+            uint256 tokenId,
+            uint128 liquidity,
+            uint256 amount0,
+            uint256 amount1
+        )
+    {
+        // optionally handle validation
+        return nonfungiblePositionManager.mint(params);
+    }
+
+    function collectLiquidityFees(
+        INonfungiblePositionManager.CollectParams memory params
+    ) returns (uint256 amount0, uint256 amount1) {
+        // optionally handle validation
+        return nonfungiblePositionManager.collect(params);
+    }
+
+    function increaseLiquidity(
+        INonfungiblePositionManager.IncreaseLiquidityParams memory params
+    ) returns (uint128 liquidity, uint256 amount0, uint256 amount1) {
+        // optionally handle validation
+        return nonfungiblePositionManager.increaseLiquidity(params);
+    }
+
+    function decreaseLiquidity(
+        INonfungiblePositionManager.DecreaseLiquidityParams memory params
+    ) returns (uint256 amount0, uint256 amount1) {
+        // optionally handle validation
+        return nonfungiblePositionManager.decreaseLiquidity(params);
     }
 }
