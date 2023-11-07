2f39167207d40f6ef2d400cb7c5486bebccca39a
Sam Sends <sbsends@gmail.com>
2023-11-06 17:11:44 -0800

    ✨  added a ERC721/ERC1155 receiver fallback

--- src/Vault.sol
+++ src/Vault.sol
@@ -50,12 +50,27 @@ contract Vault is Ownable {
     fallback() external payable {
         bytes4 functionSelector = bytes4(msg.data[:4]);
         address target = routes[functionSelector];
-
-        require(target != address(0), "Route not found");
-        require(msg.sender == owner(), "Sender not owner");
-        (bool success, ) = target.delegatecall(msg.data);
-        require(success, "Delegatecall failed");
+        if (target != address(0) && msg.sender == owner()) {
+            (bool success, ) = target.delegatecall(msg.data);
+            require(success, "Delegatecall failed");
+        }
     }
 
     receive() external payable {}
+
+    /// @dev Modifier for the fallback function to handle token callbacks.
+    modifier receiverFallback() virtual {
+        _;
+        /// @solidity memory-safe-assembly
+        assembly {
+            let s := shr(224, calldataload(0))
+            // 0x150b7a02: `onERC721Received(address,address,uint256,bytes)`.
+            // 0xf23a6e61: `onERC1155Received(address,address,uint256,uint256,bytes)`.
+            // 0xbc197c81: `onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)`.
+            if or(eq(s, 0x150b7a02), or(eq(s, 0xf23a6e61), eq(s, 0xbc197c81))) {
+                mstore(0x20, s) // Store `msg.sig`.
+                return(0x3c, 0x20) // Return `msg.sig`.
+            }
+        }
+    }
 }
